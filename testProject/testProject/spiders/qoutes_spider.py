a1e6022ebc85207c011476744ec03094d0dde27b
PuneetShrivas <puneetshrivas32@gmail.com>
2020-12-18 11:50:00 +0530

    Added containers

--- testProject/testProject/spiders/qoutes_spider.py
+++ testProject/testProject/spiders/qoutes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import TestprojectItem
 
 
 class QuoteSpider(scrapy.Spider):
@@ -8,14 +9,18 @@ class QuoteSpider(scrapy.Spider):
     }
 
     def parse(self, response, **kwargs):
+
+        items = TestprojectItem()
+
         all_div_quotes = response.css('div.quote')
 
         for quotes in all_div_quotes:
             title = quotes.css('span.text::text').extract()
             author = quotes.css('.author::text').extract()
             tag = quotes.css('.tag::text').extract()
-            yield {
-                'title': title,
-                'author': author,
-                'tag': tag
-            }
+
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
